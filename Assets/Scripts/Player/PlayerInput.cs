f5a009e1f898c5cd3cea109d374a20e1eae1c1f6
ryviuszero <ryviuszero@gmail.com>
2026-02-01 20:19:00 +0800

    08_IS_URTS Camera Zoom

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -1,14 +1,74 @@
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerInput : MonoBehaviour
 {
     [SerializeField] private Transform cameraTarget;
+    [SerializeField] private CinemachineCamera cinemachineCamera;
     [SerializeField] private float keyboardPanSpeed = 5;
+    [SerializeField] private float zoomSpeed = 1;
+    [SerializeField] private float minZoomDistance = 7.5f;
+
+    private CinemachineFollow cinemachineFollow;
+    private float zoomStartTime;
+    private Vector3 startingFollowOffset;
+
+    private void Awake()
+    {
+        if ( !cinemachineCamera.TryGetComponent(out cinemachineFollow) )
+        {
+            Debug.LogError("Cinemachine Camera did not have CinemachineFollow. Zoom functionality will not work!");
+        }
+
+        startingFollowOffset = cinemachineFollow.FollowOffset;
+    }
 
     private void Update()
     {
+        HandlePanning();
+        HandleZoom();
+    }
+    private void HandleZoom()
+    {
+        if (ShouldSetZoomStartTime())
+        {
+            zoomStartTime = Time.time;
+        }
 
+        float zoomTime = Mathf.Clamp01((Time.time - zoomStartTime) * zoomSpeed); 
+        Vector3 targetFollowOffset;
+
+        if(Keyboard.current.endKey.isPressed)
+        {
+            targetFollowOffset = new Vector3(
+                cinemachineFollow.FollowOffset.x,
+                minZoomDistance,
+                cinemachineFollow.FollowOffset.z
+            );
+        }
+        else
+        {
+            targetFollowOffset = new Vector3(
+                cinemachineFollow.FollowOffset.x,
+                startingFollowOffset.y,
+                cinemachineFollow.FollowOffset.z
+            );
+        }
+        cinemachineFollow.FollowOffset = Vector3.Slerp(
+            cinemachineFollow.FollowOffset,
+            targetFollowOffset,
+            zoomTime
+        );
+    }
+
+    private bool ShouldSetZoomStartTime()
+    {
+        return Keyboard.current.endKey.wasPressedThisFrame || Keyboard.current.homeKey.wasPressedThisFrame;
+    }
+
+    private void HandlePanning()
+    {
         Vector2 moveAmount = Vector2.zero;
         
         if(Keyboard.current.upArrowKey.isPressed)
@@ -30,6 +90,5 @@ public class PlayerInput : MonoBehaviour
         }
 
         cameraTarget.position += new Vector3(moveAmount.x, 0, moveAmount.y) * Time.deltaTime;
-        
     }
 }
